3b40057ac80bd24084160ba0c1a121932a66f525
elifogreniyor <elfogreniyor@gmail.com>
2023-05-21 01:22:54 +0300

    delete code and start project

--- lib/main.dart
+++ lib/main.dart
@@ -13,46 +13,8 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueAccent,
         body: SafeArea(
-          child: Column(
-            //mainAxisAlignment: MainAxisAlignment.center, 
-            //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //aralarina bosluk birakarak
-            //verticalDirection: VerticalDirection.up,
-            //crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.yellowAccent,
-                child: Text('Container 1'),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.purpleAccent,
-                child: Text('Container 2'),
-              ),
-              Container(
-                height: 100.0,
-                color: Colors.greenAccent,
-                child: Text('Container 3'),
-              ),
-              Container(
-                width: double.infinity,
-                height: 10.0,
-                color: Colors.amberAccent
-              ),
-            ],
           ),
-        ),
       ),
     );
   }
 }
-
-
-//margin: EdgeInsets.all(20.0), //Tum kenarlardan 20 pixel ayirmak
-//margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-//margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-//margin: EdgeInsets.only(left:10.0), //outside of your widget
-//padding: EdgeInsets.all(20.0), //inside of your widget
