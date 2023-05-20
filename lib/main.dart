d9efdc395da3fb221acd27e3e63deaed364b5444
elifogreniyor <elfogreniyor@gmail.com>
2023-05-21 01:19:48 +0300

    some problem fixed

--- lib/main.dart
+++ lib/main.dart
@@ -13,20 +13,19 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blueAccent,
         body: SafeArea(
-          child: Row(
+          child: Column(
             //mainAxisAlignment: MainAxisAlignment.center, 
             //mainAxisAlignment: MainAxisAlignment.spaceEvenly, //aralarina bosluk birakarak
             //verticalDirection: VerticalDirection.up,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
+            //crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
-                height: 100.0,
+                width: 100.0,
                 color: Colors.yellowAccent,
                 child: Text('Container 1'),
               ),
               SizedBox(
-                //height: 20.0, //column olunca bi anlami var
-                width: 10.0, //row olunca bi anlami var
+                height: 20.0,
               ),
               Container(
                 height: 100.0,
